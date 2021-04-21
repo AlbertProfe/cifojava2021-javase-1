d086f4b395a1697f6298628a07397872f91de126
admin CIFO <admin CIFO@DESKTOP-NG7OTN7>
2021-04-21 11:41:19 +0200

    first commit

---
+++ Exemple1/src/test/Test.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Scanner reader = new Scanner(System.in);
+
+		Person joan = new Person();
+
+		System.out.println("Tell me your name?");
+		String name = reader.nextLine();
+		System.out.println("Tell me your age?");
+		int age = reader.nextInt();
+
+		System.out.println("Hello, " + name);
+
+		if (age >= 18) {
+			System.out.println(name + " you are an adult!!");
+
+		} else {
+			System.out.println(name + " you are NOT an adult!!");
+		}
+
+	}
+
+	public static void sum() {
+	}
+
+}
