c382e9249a1551e67ac144c8269527c4f16af451
admin CIFO <admin CIFO@DESKTOP-NG7OTN7>
2021-04-26 11:30:41 +0200

    strings, whiles, methods, index, etc..

--- WordsOperations/src/wordsoperations/Init.java
+++ WordsOperations/src/wordsoperations/Init.java
@@ -3,17 +3,18 @@ package wordsoperations;
 import java.util.Scanner;
 
 public class Init {
-
+	//scope #1
 	public static void main(String[] args) {
 
-		
+		//scope #2
 		Scanner reader = new Scanner (System.in);
 		System.out.println("Write some string ...");
 		String word = reader.nextLine();
 		
-		Operations.print(word);
-		Operations.linebreak();
-		Operations.printString(word);
+		//Operations.print(word);
+		//Operations.linebreak();
+		//Operations.printString(word);
+		Operations.printStringH(word);
 		
 		
 		System.out.println("\nBye bye my friend...");
--- WordsOperations/src/wordsoperations/Operations.java
+++ WordsOperations/src/wordsoperations/Operations.java
@@ -1,30 +1,34 @@
 package wordsoperations;
 
 public class Operations {
-
+	//scope #8
+	//int pepe = 19;
+	
 	public static void print(String wordtoPrint) {
-
+		//scope #3
 		System.out.print(wordtoPrint);
 	}
-
+	
 	public static void print(char chartoPrint) {
-
+		//scope #4
 		System.out.print(chartoPrint);
 	}
-
+	
 	public static void linebreak() {
+		//scope #5
 		System.out.print("\n");
 	}
-
+	
 	public static void printString(String wordtoPrint) {
-
+		//scope #6
 		int i = 0;
 		linebreak();
 		int sizeWordtoPrint = wordtoPrint.length();
+		
 		while (i < sizeWordtoPrint) {
-			
-			print("counter: " + (i) + " - " );
-			print("loop number: " + (i + 1) + " - " );
+			//scope #7
+			print("counter: " + (i) + " - ");
+			print("loop number: " + (i + 1) + " - ");
 			print("letter: " + wordtoPrint.charAt(i));
 			linebreak();
 
@@ -33,4 +37,29 @@ public class Operations {
 		}
 
 	}
+	
+	
+	public static void printStringH (String wordtoPrint) {
+		//scope #9
+		int i = 0;
+		linebreak();
+		int sizeWordtoPrint = wordtoPrint.length();
+		
+		while (i < sizeWordtoPrint) {
+		print(wordtoPrint.charAt(i) + " " );
+		i++; // i = i +1;
+
+		}
+	}
+	
+	public static void printStringV (String wordtoPrint) {
+		//scope #10
+	}
+	
+	public static void printStringHG (String wordtoPrint) {
+		//scope #11
+	}
+	
+	
+	
 }
