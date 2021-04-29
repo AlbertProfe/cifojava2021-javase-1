2b220db40c0085c4dd3bc8637f3fb643edd8151a
admin CIFO <admin CIFO@DESKTOP-NG7OTN7>
2021-04-29 13:01:18 +0200

    account management & others

--- WordsOperations5/src/wordsoperations/AccountCreation.java
+++ WordsOperations5/src/wordsoperations/AccountCreation.java
@@ -1,57 +1,52 @@
 package wordsoperations;
 
 public class AccountCreation {
-	
 
+	public static String createUser(String createUser) {
+		// scope #5
 
-		public static String createUser(String createUser) {
-			// scope #5
+		int blank = createUser.indexOf(" ");
+		String user = createUser.substring(0, blank);
 
-			int blank = createUser.indexOf(" ");
-			String user = createUser.substring(0, blank);
+		return user;
+	}
 
-			return user;
-		}
+	public static String createEmail(String createEmail) {
+		// scope #6
 
-		public static String createEmail(String createEmail) {
-			// scope #6
+		int blank = createEmail.indexOf(" ");
+		String surname = createEmail.substring(blank + 1, createEmail.length());
+		String email = createEmail.charAt(0) + surname + "@java.com";
 
-			int blank = createEmail.indexOf(" ");
-			String surname = createEmail.substring(blank + 1, createEmail.length());
-			String email = createEmail.charAt(0) + surname + "@java.com";
+		return email;
+	}
 
-			return email;
-		}
+	public static String createPassword() {
+		// scope #7
 
-		public static String createPassword() {
-			// scope #7
+		String password = "";
 
-			String password = "";
+		password = password + Utils.createCharRandom() + Utils.createIntRandom(10000) + Utils.createCharRandom()
+				+ Utils.createCharRandom() + Utils.createIntRandom(596);
 
-			password = password + Utils.createCharRandom() + Utils.createIntRandom(10000)
-			+ Utils.createCharRandom() + Utils.createCharRandom()
-					+ Utils.createIntRandom(596);
+		return password;
+	}
 
-			return password;
-		}
+	public static void printUserAccount(String user, String email, String password) {
+		// scope #8
 
-		public static void printUserAccount(String user, String email, String password) {
-			// scope #8
+		System.out.println("User Account: ");
+		System.out.println("User : " + user);
+		System.out.println("Email: " + email);
+		System.out.println("Password: " + password);
 
-			System.out.println("User Account: ");
-			System.out.println("User : " + user);
-			System.out.println("Email: " + email);
-			System.out.println("Password: " + password);
+	}
 
-		}
+	public static String accountToString(String user, String email, String password) {
 
-		public static String accountToString (String user, String email, String password) {
-			
-			//to-do
-			
-			return null;
-			
-			
-		}
+		String accountToString = user + "-" + email + "-" + password;
+
+		return accountToString;
+
+	}
 }
-
