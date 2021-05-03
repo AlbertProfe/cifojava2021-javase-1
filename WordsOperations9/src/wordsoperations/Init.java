a35daa419748d61a0d5dc88616c72ce94940438f
admin CIFO <admin CIFO@DESKTOP-NG7OTN7>
2021-05-03 11:35:31 +0200

    commit one

---
+++ WordsOperations9/src/wordsoperations/Init.java
@@ -0,0 +1,15 @@
+package wordsoperations;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Init {
+	// scope #1
+	public static void main(String[] args) {
+
+		Controller.AppStarting();
+
+		System.out.println("\nBye bye my friend...");
+
+	}
+}
